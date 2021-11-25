f1bf02d49c018b4a07442435cd17c0483a9b4c87
Uhmopt <qethum@gmail.com>
2021-11-25 21:08:11 +1100

    [New] packages

--- src/pages/packages/index.js
+++ src/pages/packages/index.js
@@ -1,12 +1,18 @@
 import Layout from "layouts";
 import PageHeader from "layouts/page/PageHeader";
 import React from "react";
-import image from "assets/img/back/coin-board.jpg";
+import image from "assets/img/back/1.jpg";
+import PriceSection from "pages/welcome/PriceSection";
+import LatestWithdrawsSection from "pages/welcome/LatestWithdrawsSection";
+import FriendsSection from "pages/welcome/FriendsSection";
 
 export default function Packages() {
   return (
     <Layout fullWidth>
       <PageHeader image={image} title="OUR PACKAGES" subTitle="PRICING TABLE" />
+      <PriceSection />
+      <LatestWithdrawsSection />
+      <FriendsSection />
     </Layout>
   );
 }
